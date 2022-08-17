db90553577431017f2faad903d6a60a9be8c9dd7
Frank <ingfralch@gmail.com>
2022-08-17 17:12:05 -0500

    ubicacion

--- App.js
+++ App.js
@@ -2,15 +2,21 @@ import React, { useState, useEffect, useRef } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, TextInput, ActivityIndicator } from 'react-native';
 import { Camera, CameraType } from 'expo-camera';
 import * as FileSystem from 'expo-file-system';
+import * as Location from 'expo-location';
+import MapView from 'react-native-maps';
+import { Marker } from 'react-native-maps';
 
 export default function App() {
   const [permiso, fijarPermiso] = useState(null);
   const [dni, fijarDni] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [direccion, setDireccion] = useState(null);
 
   const camaraRef = useRef(null);
   const [foto, setFoto] = useState(null);
 
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
 
   useEffect(() => {
@@ -19,6 +25,44 @@ export default function App() {
       fijarPermiso(status === 'granted');
     })();
   }, []);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  const obtenerDireccion = async (lat, lng) => {
+    const Getdireccion  = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=AIzaSyBm29s27MlvC9J4LkHW5gftm8QC-Pim48I`)
+    const rpt = await Getdireccion.json();
+    const direccion = rpt.results[0].formatted_address;
+    const distrito = rpt.results[0].address_components[2].long_name;
+    let expresionRegular = /\s*,\s*/;
+    let direccionFinal = direccion.split(expresionRegular)[0] + ',' + distrito;
+    setDireccion(direccionFinal);
+    // console.log(direccionArray[0]);
+    console.log(direccionFinal);
+  }
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    let { coords } = location;
+    // text = JSON.stringify(location);
+    // text = `Latitude: ${coords.latitude} Longitude: ${coords.longitude}`;
+    obtenerDireccion(coords.latitude, coords.longitude);
+  }
+  
+ 
+
 
   if (permiso === null) {
     return <View />;
@@ -34,7 +78,7 @@ export default function App() {
       const base64 = await FileSystem.readAsStringAsync(foto.uri, { encoding: 'base64' });
       // console.log(base64);
       setLoading(true);
-      const response = await fetch('http://192.168.1.19/cre/cli/subir_imagen', {
+      const response = await fetch('http://192.168.1.5/cre/cli/subir_imagen', {
         method: 'POST',
         headers: {
           Accept: 'application/json',
@@ -53,6 +97,10 @@ export default function App() {
     }
     setLoading(false);
   };
+
+
+
+  
   return (
     
       loading ?   
@@ -73,7 +121,15 @@ export default function App() {
             {/* <Text style={styles.text} onPress={() => Alert.alert('Simple Button pressed')}> ENVIAR </Text>            */}
             <Text style={styles.text} onPress={() => tomarFoto()}> ENVIAR </Text>
           </TouchableOpacity>
-
+          <View style={styles.direccion}>  
+            <Text>{direccion}</Text>
+            <View>
+            <MapView>
+             
+            </MapView>
+            </View>
+          </View>
+          
         </View>
       </Camera>
       </View>
@@ -125,4 +181,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around',
     padding: 10,
   },
+  direccion: {
+    flex: 1,
+  }
 });
